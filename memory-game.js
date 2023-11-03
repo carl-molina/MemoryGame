bb848c7e4cffef1d7ba3515fba00a4b0cfaff153
Carl Molina <carlandrew.molina@gmail.com>
2023-11-03 13:54:34 -0700

    Add guess/best counters and number of cards option

--- memory-game.js
+++ memory-game.js
@@ -1,20 +1,77 @@
 "use strict";
 
 const FOUND_MATCH_WAIT_MSECS = 1000;
-const COLORS = [
-  "red", "blue", "green", "orange", "purple",
-  "red", "blue", "green", "orange", "purple",
-];
+let COLORS_SINGLE = [];
+let COLORS_DOUBLE = [];
 
-const colors = shuffle(COLORS);
-const winCount = colors.length;
+const numOfTiles = document.querySelector('#play-start');
+document.addEventListener('DOMContentLoaded', function(event) {
+  // console.log(`The DOM has loaded!`);
+  numOfTiles.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const gameArea = document.querySelector('div.game-area');
+    gameArea.scrollIntoView();
+    COLORS_SINGLE = [];
+    COLORS_DOUBLE = [];
+    const num = document.querySelector('input[name="num-of-tiles"]');
+    const numberOfTiles = Number(num.value);
+    // console.log(`Submit works!`);
+    for (let i = 0; i < numberOfTiles / 2; i++) {
+      let newColor = randomColor();
+      COLORS_SINGLE.push(newColor);
+    }
+    // console.log(`This is COLORS_SINGLE: ${COLORS_SINGLE}`);
+    COLORS_DOUBLE = [...COLORS_SINGLE, ...COLORS_SINGLE];
+    colors = shuffle(COLORS_DOUBLE);
+    winCount = colors.length;
+    createCards(colors);
 
+    // When player wants to make new game w/ different number of cards.
+    const oldCards = document.querySelectorAll('#game div');
+    for (let i = 0; i < oldCards.length; i++) {
+      oldCards[i].remove();
+    }
+    cardCount = 0;
+    firstCard = null;
+    freezePlay = false;
+    colors = shuffle(COLORS_DOUBLE);
+    winCount = colors.length;
+    createCards(colors);
+    guessCount = 0;
+    const guessCounter = document.getElementById('guess-count-num');
+    guessCounter.innerText = null;
+  });
+});
+
+
+function randomColor() {
+  const letters = '0123456789ABCDEF';
+  let hex = '#';
+  for (let i = 0; i < 6; i++) {
+    hex += letters[Math.floor(Math.random() * 16)];
+  }
+  return hex;
+}
+
+let colors = shuffle(COLORS_DOUBLE);
+let winCount = colors.length;
 let cardCount = 0;
 let firstCard = null;
 let freezePlay = false;
-
+let guessCount = 0;
+let lowestScore = Infinity;
 
 createCards(colors);
+
+// retrieve from localStorage
+const savedRecord = JSON.parse(localStorage.getItem('record')) || [];
+for (let obj of savedRecord) {
+  if (Number(obj.record) < lowestScore) {
+    lowestScore = obj.record;
+  }
+}
+const bestRecord = document.getElementById('best-record-num');
+bestRecord.innerText = lowestScore;
 
 
 function shuffle(items) {
@@ -45,7 +102,7 @@ function createCards(colors) {
 function flipCard(card) {
   card.setAttribute('data-state', 'face-up');
   const cardColor = card.getAttribute('data-color');
-  console.log(`This is cardColor: ${cardColor}`);
+  // console.log(`This is cardColor: ${cardColor}`);
   card.style.backgroundColor = cardColor;
 }
 
@@ -67,34 +124,67 @@ function handleCardClick(evt) {
   } else if (checkStatus === 'face-down' && !firstCard) {
     flipCard(cardClicked);
     firstCard = cardClicked;
-    console.log(`This is the firstCard: ${firstCard}`);
+    // console.log(`This is the firstCard: ${firstCard}`);
     return;
   } else if (checkStatus === 'face-down' && !!firstCard) {
-    console.log(`This is the second card`);
+    // console.log(`This is the second card`);
     flipCard(cardClicked);
     const colorCheck1stCard = firstCard.getAttribute('data-color');
     const colorCheck2ndCard = cardClicked.getAttribute('data-color');
+
+    guessCount++;
+    const guessCounter = document.getElementById('guess-count-num');
+    guessCounter.innerText = guessCount;
+
     if (colorCheck1stCard === colorCheck2ndCard) {
       firstCard.setAttribute("data-state", "matched");
       cardClicked.setAttribute("data-state", "matched");
-      console.log(`Two cards matched! Resetting firstCard.`);
+      // console.log(`Two cards matched! Resetting firstCard.`);
       firstCard = null;
       cardCount += 2;
 
       if (cardCount === winCount) {
-        console.log(`You won the game!`);
+        // console.log(`You won the game!`);
+        if (guessCount < lowestScore) {
+          lowestScore = guessCount;
+          const bestRecord = document.getElementById('best-record-num');
+          bestRecord.innerText = lowestScore;
+
+          // save to localStorage
+          savedRecord.push({record: bestRecord.innerText});
+          localStorage.setItem("record", JSON.stringify(savedRecord));
+        }
       }
     } else if (colorCheck1stCard !== colorCheck2ndCard) {
       freezePlay = true;
       flipCard(cardClicked);
-      console.log(`The cards did NOT match! Restting them.`);
+      // console.log(`The cards did NOT match! Resetting them.`);
       setTimeout(function() {
         unflipCard(firstCard);
         unflipCard(cardClicked);
         firstCard = null;
         freezePlay = false;
-        console.log(`Unflipping firstCard in 1 second.`);
+        // console.log(`Unflipping firstCard in 1 second.`);
       }, FOUND_MATCH_WAIT_MSECS);
     }
   }
 }
+
+const resetGame = document.querySelector('#play-again');
+
+resetGame.addEventListener('click', function(e) {
+  e.preventDefault();
+  const oldCards = document.querySelectorAll('#game div');
+  for (let i = 0; i < oldCards.length; i++) {
+    oldCards[i].remove();
+  }
+  cardCount = 0;
+  firstCard = null;
+  freezePlay = false;
+  colors = shuffle(COLORS_DOUBLE);
+  winCount = colors.length;
+  createCards(colors);
+  guessCount = 0;
+  const guessCounter = document.getElementById('guess-count-num');
+  guessCounter.innerText = null;
+});
